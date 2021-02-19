42cec5d48704307329bd107f1131565b495fe395
Ahmed <k200388@l06v116.khifast.nu.edu.pk>
2021-02-19 11:54:48 +0500

    task3

---
+++ task3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+
+void ruser(int min, int acc, char scode){
+	cout << "Account Number : " << acc << endl;
+	cout << "Minutes Used : " << min << endl;
+	cout << "Amount Due : " << (min - 75) * 0.10<< endl;
+	cout << "Account Type : Regular\n";
+}
+void puser(int day, int night, int acc, char scode) {
+	cout << "Account Number : " << acc << endl;
+	cout << "Minutes Used : " << min << endl;
+	cout << "Amount Due : " << (day - 75) * 0.10 + (nigt - 100) * 
+	<< endl;
+}
+
+int main() {
+	int acc;
+	char scode;
+	int min;
+	int day;
+	int night;
+	cout << "Enter account number : " << endl;
+	cin >> acc;
+	cout << "Enter service code : " <<endl;
+	cin >> scode;
+	if (scode == 'r' || scode  == 'R') {
+		cout << "Enter minutes used : " << endl;
+		cin >> min;
+		ruser(min);
+	}
+	else if (scode == 'p' || scode == 'P') {
+		cout << "Enter minutes used in day : " <<endl;
+		cin >> day;
+		cout << "Enter minutes used in night : " << endl;
+		cin >> night;
+		puser(day, night);
+	}
+}
